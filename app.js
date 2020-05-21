cbf9a67877b7cfa2cac553d2596e5c594b16290f
的場郁美 <>
2020-05-21 16:52:21 +0900

    完成

--- app.js
+++ app.js
@@ -1,13 +1,13 @@
 const API_KEY = 'AIzaSyBz7YdoVfisioa6-Php0HEYYHjU-kz4O50';
 
 // 検索結果１件を表示するカードの雛形
-const Card = () => `
+const Card = ({ src, title, description }) => `
     <div class="col-4 mb-5">
         <div class="card">
-            <img src="https://i.ytimg.com/vi/Dx_fKPBPYUI/hqdefault.jpg" class="card-img-top" alt="...">
+            <img src="${src}" class="card-img-top" alt="...">
             <div class="card-body">
-            <h5 class="card-title">米津玄師 MV「LOSER」</h5>
-            <p class="card-text">Honda「JADE」CM 5th Single「LOSER / ナンバーナイン」</p>
+            <h5 class="card-title">${title}</h5>
+            <p class="card-text">${description}</p>
             </div>
         </div>
     </div>
@@ -15,7 +15,7 @@ const Card = () => `
 
 $('#search-btn').on('click', () => {
 
-    // $('#results').append(Card());
+    
 
     // 検索ワードの取得
     const word = $('#search-word').val()
@@ -29,10 +29,25 @@ $('#search-btn').on('click', () => {
             part: 'snippet',
             q: word,
             maxResults: '50',
-            // type: 'video',
+            type: 'video',
         }
     }).done((response) => {
-        console.log(response)
+
+        for(let i = 0; i < response.items.length; i++) {
+            let url = response.items[i].snippet.thumbnails.high.url
+            let titleText = response.items[i].snippet.title
+            let descriptionText = response.items[i].snippet.description
+
+
+
+            $('#results').append(
+                Card({ src: url, title: titleText, description: descriptionText,
+            }));
+
+            // console.log(array)
+        }
+
+
 
     }).fail((error) => {
         console.log(error)
