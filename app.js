52f556b229953d12592b8309b7f72dd3f3efe32f
的場郁美 <>
2020-05-25 16:12:33 +0900

    iTunes_API完成

--- app.js
+++ app.js
@@ -1,14 +1,13 @@
-const API_KEY = 'AIzaSyBz7YdoVfisioa6-Php0HEYYHjU-kz4O50';
-
 // 検索結果１件を表示するカードの雛形
-const Card = ({ src, title, description, videoId }) => `
+const Card = ({ src, title, artist, musicId }) => `
     <div class="col-4 mb-5">
-        <a href="https://www.youtube.com/watch?v=${videoId}" target="_blank">
+        <a href="${musicId}" target="_blank">
             <div class="card">
                 <img src="${src}" class="card-img-top" alt="...">
                 <div class="card-body">
-                <h5 class="card-title">${title}</h5>
-                <p class="card-text">${description}</p>
+                    <h5 class="album-title">${title}</h5>
+                    <p class="artist">${artist}</p>
+                    <button type="button" class="btn btn-primary">Detail</button>
                 </div>
             </div>
         </a>
@@ -16,50 +15,42 @@ const Card = ({ src, title, description, videoId }) => `
 `;
 
 $('#search-btn').on('click', () => {
-    
 
-    // サムネイル表示をクリア
     $("#results").empty();
+
     
     // 検索ワードの取得
-    const word = $('#search-word').val()
-
-    // Ajax開始
+    const word = $('#search-word').val();
+    // alert(word)
+    
+    // データを出すのがajax
     $.ajax({
-        url:'https://www.googleapis.com/youtube/v3/search',
+        url: 'https://itunes.apple.com/search',
         type: 'GET',
+        dataType: 'jsonp', // 通信結果のデータ形式(例：html, json, jsonp, xml)
         data: {
-            key: API_KEY,
-            part: 'snippet',
-            q: word,
+            term: word,
+            country: 'jp',
             maxResults: '50',
-            type: 'video',
+
+
         }
+        
     }).done((response) => {
+        for(let i = 0; i < response.results.length; i++) {
+            let url = response.results[i].artworkUrl100
+            let titleText = response.results[i].collectionName
+            let artistText = response.results[i].artistName
+            let id = response.results[i].collectionViewUrl
+            $('#results').append(Card({src: url, title: titleText, artist: artistText, musicId: id}));
 
-        for(let i = 0; i < response.items.length; i++) {
-            let url = response.items[i].snippet.thumbnails.high.url
-            let titleText = response.items[i].snippet.title
-            let descriptionText = response.items[i].snippet.description
-            let id = response.items[i].id.videoId
-
-            console.log(id)
-
-            $('#results').append(
-                Card({ src: url, title: titleText, description: descriptionText, videoId: id
-            }));
-
-
+            console.log(url)
         }
-
-
-
 
 
     }).fail((error) => {
         console.log(error)
-    })
 
-    
+    })
 
 })
